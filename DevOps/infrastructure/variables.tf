16b01a06c68a557e944fd4c4cf7d5ddf592c60bb
Andre Regino <reginopanai@gmail.com>
2026-02-28 01:12:11 -0700

    Fixed confusion with folder structure

--- DevOps/infrastructure/variables.tf
+++ DevOps/infrastructure/variables.tf
@@ -4,6 +4,12 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "aws_profile" {
+  description = "AWS CLI profile to use for deployment"
+  type        = string
+  default     = "default"
+}
+
 variable "environment" {
   description = "Environment name"
   type        = string
